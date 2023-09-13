a2e29fa993904c0288f2bf4081f9c5d81005cc85
Samrat798 <majumdersamrat26@gmail.com>
2023-09-13 23:43:47 +0530

    aster

---
+++ 01.intro.js
@@ -0,0 +1,22 @@
+/* 
+INTRODUCTION TO JAVASCRIPT
+01. What is Javascript?
+==> JavaScript is High-Level Object-Oriented programming language.
+
+02. What is High-Level means?
+==> In a High-Level language like Javascript we don't have to worry about
+complex stuff like memory management.
+
+03. The role of javascript in web development?
+==> The three major pillars of web development is HTML, CSS and JavaScript.
+    In a web page lot of things going on behind the scenes. The content of the web page represented by HTML.
+    With the help of CSS we design the content which represented by HTML.
+    At last the Javascript help us to create dynamic content, get data from a remote server, dynamic effect, load data in background etc.
+    We can also use javascript with on the server side using Node.js.
+
+04. What can we with Javascript?
+==> For a big web web site like amazon, netflix, flipkart we can't hard code everything because the data change time to time
+    so with the help of javascript and API we fetch the data from a remote server and load it dynamically in our page.
+    We can build a web application on the browser. We all hard about REACT, ANGULAR and VIW this are the popular frame work 
+    build on top of javascript.
+*/
